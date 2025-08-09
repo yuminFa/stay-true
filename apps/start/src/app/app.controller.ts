7e8504f2113120ddb4d8e66b94d3306535ffc6cf
jungjin.lee <jungjin.lee@a-ha.io>
2025-08-09 10:24:59 +0900

    hbs 적용

--- apps/start/src/app/app.controller.ts
+++ apps/start/src/app/app.controller.ts
@@ -1,12 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getData() {
-    return this.appService.getData();
-  }
+    @Get()
+    @Render('index')
+    getData() {
+        return { message: 'Hello world!' };
+    }
 }
--- apps/start/src/main.ts
+++ apps/start/src/main.ts
@@ -5,15 +5,20 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    app.useStaticAssets(join(__dirname, '..', 'public'));
+    app.setBaseViewsDir(join(__dirname, '..', 'views'));
+    app.setViewEngine('hbs');
+
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();

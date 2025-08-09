c00db56969a59a3abcc30ad7d4720cb91435086b
jungjin.lee <jungjin.lee@a-ha.io>
2025-08-10 00:10:05 +0900

    python 설정

--- apps/start/src/app/app.module.ts
+++ apps/start/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from '@stay-simple/prisma';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from '@stay-simple/prisma';
 
 @Module({
     imports: [PrismaModule],
